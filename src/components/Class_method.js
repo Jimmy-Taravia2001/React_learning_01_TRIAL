c5f982f25ac424f794c2de4265b0097ce6c56765
Jimmy Taravia <taraviajimmy01@gmail.com>
2022-10-22 23:18:55 +0530

    learn_react_01

---
+++ src/components/Class_method.js
@@ -0,0 +1,9 @@
+import React,{Component} from "react";
+
+class Welcome extends Component{
+    render(){
+        return <h1>Class method has overtaken function, JAsper </h1>;
+    }
+};
+
+export default Welcome
---
+++ src/components/function_method.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+// function Greet(){
+//     return <h1>Hello JimmyJasper</h1>;
+// }
+const Great = () => <h1>hello jasper you replaced jimmy</h1> 
+
+
+export default Great;
