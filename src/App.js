e08ae04ca903c31222440e41e90fd1e7f98e64b9
KetulChhaya <chhayaketul@gmail.com>
2022-10-31 00:34:38 +0530

    about section

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import logo from "./logo.svg";
 import "./App.css";
 import Navbar from "./components/Navbar/Navbar";
 import Header from "./containers/Header/Header";
+import About from "./components/About/About";
 
 function App() {
   return (
     <div className="App">
       <Navbar />
       <Header />
+      <About />
     </div>
   );
 }
